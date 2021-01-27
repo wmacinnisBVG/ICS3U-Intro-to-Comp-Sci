92bad1dbd2971dc6ba1ef78b3d82184c55c810cd
William MacInnis <wmacinnis@bayviewglen.ca>
2021-01-27 10:44:05 -0500

    Finished Day 1 Homework

---
+++ Unit One - Introduction to Java/src/week1/DayOneHomework.java
@@ -0,0 +1,51 @@
+package week1;
+
+public class DayOneHomework {
+    public static void main(String[] args) {
+        System.out.println("\nDay 1 Homework - Display to Console");
+
+        // Question One
+        System.out.println("\n1.\tA program to print your initials in block letters. Your output could look like:");
+        System.out.println("\t JJJJJJJJJJ\t\t   A   \t\t  CC  ");
+        System.out.println("\t     J    \t\t A   A\t\t C  C");
+        System.out.println("\t     J    \t\tA     A\t\tC     ");
+        System.out.println("\t    J    \t\tAAAAAAA\t\tC     ");
+        System.out.println("\tJ    J    \t\tA     A\t\t C  C");
+        System.out.println("\t JJJ       \t\tA     A\t\t  CC ");
+
+        // Question Two
+        System.out.println("\n2.\tDesign a simple picture and print it to the console. Examples would be a small house or a stick person. You may want to use \\t for tab.");
+        System.out.println("      __");
+        System.out.println("     /  \\");
+        System.out.println("    /    \\");
+        System.out.println("   /      \\");
+        System.out.println("  /        \\");
+        System.out.println(" /          \\");
+        System.out.println("/            \\");
+        System.out.println("--------------");
+        System.out.println("|            |");
+        System.out.println("|   ______   |");
+        System.out.println("|   |    |   |");
+        System.out.println("|   |    |   |");
+        System.out.println("|   |   o|   |");
+        System.out.println("|   |    |   |");
+
+        //Question Three
+        System.out.println("\n3.\tA program to print out your mailing address.");
+        System.out.println("wmacinnis@bayviewglen.ca");
+
+        //Question Four
+        System.out.println("\n4.\tOur lady of Mercy Hospital prints billing statements for patients when they are ready to leave the hospital. Write a program that prints a heading for each statement as follows:");
+        System.out.println("/////////////////////////////////////////////////////////////////////");
+        System.out.println("/                                                                   /");
+        System.out.println("/                   Our Lady of Mercy Hospital                      /");
+        System.out.println("/                   --------------------------                      /");
+        System.out.println("/                                                                   /");
+        System.out.println("/                                                                   /");
+        System.out.println("/                        1306 Central City                          /");
+        System.out.println("/                      Phone (416) 555-1212                         /");
+        System.out.println("/                                                                   /");
+        System.out.println("/////////////////////////////////////////////////////////////////////");
+
+    }
+}
