360a52c49d49f1650266bb36cb584a721ced07c3
William MacInnis <wmacinnis@bayviewglen.ca>
2021-01-27 12:16:02 -0500

    Finished question 2

--- Unit One - Introduction to Java/src/week1/DayTwoHomework.java
+++ Unit One - Introduction to Java/src/week1/DayTwoHomework.java
@@ -6,14 +6,26 @@ public class DayTwoHomework {
         
         //Question One
         System.out.println("\n1.\tWrite program that can calculate the area of circle. You will need to create variables for the radius and the area.");
-        questionOne(15); 
+        areaOfaCircle(15); 
+        System.out.println("\n\n2.\tWrite a program that can calculate the volume of a sphere.");
+        volumeOfSphere(20);
+        System.out.println("\n\n3.\tCreate a program that can solve the equation y = ax2+bx+c given a, b, c and x.");
     }
 
-    private static void questionOne(double radius) {
+    private static void volumeOfSphere(double radius) {
+        //Volume of a sphere 4/3πr3
+        double volume = 1.33333333333*3.14*Math.pow(radius,3);
+        System.out.print("\nThe volume of a sphere with a radius of ");
+        System.out.print(radius);
+        System.out.print(" is: ");
+        System.out.print(volume);
+    }
+
+    private static void areaOfaCircle(double radius) {
         //Area of a circle A=πr2
         double area; 
         area = 3.14*radius*radius;
-        System.out.print("The area of a circle with a radius of ");
+        System.out.print("\nThe area of a circle with a radius of ");
         System.out.print(radius);
         System.out.print(" is: ");
         System.out.print(area);
